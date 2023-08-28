2a2c8a15e21573e4843d439b652cbe8feca82dab
Will Smith <williamqsmith11@gmail.com>
2023-08-28 16:50:41 -0700

    a

--- crates/bundler/src/bundle_signer.rs
+++ crates/bundler/src/bundle_signer.rs
@@ -1,11 +1,25 @@
+use shared::Bundle;
 use sim::Simulator;
+use this_error::Error;
 
-pub enum BundleSignResults {
-    Approve(),
-    Failed()
+#[derive(Error)]
+pub enum BundleSigningError{
+    #[error("Failed to simulate bundle: {0:#?}")]
+    SimulationError
+    #[error("failed to sign bundle: {0:#?}")]
+    SigningError
 }
+
+
 /// deals with verifying the bundle
 pub struct BundleSigner<S: Simulator> {
-    sim: S
+    sim: S,
+    // edsca key. in future will bls key
+    key: Wallet<SigningKey>
 }
 
+impl<S: Simulator> BundleSigner<S> {
+    async fn sim_and_sign_bundle(&self) -> Result<Bundle, BundleSigningError> {
+        todo!()
+    }
+}
