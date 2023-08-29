c2e42c76e0dfb6719a48e403d1bae6983fe8d74b
Will Smith <williamqsmith11@gmail.com>
2023-08-28 23:55:03 -0700

    yeet'

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 use clap::Parser;
 use ethers_core::rand::rngs::ThreadRng;
 use ethers_providers::{Http, Provider};
+use ethers_reth::RethMiddleware;
 use ethers_signers::LocalWallet;
 use guard_network::{config::SecretKey, NetworkConfig, PeersConfig};
 use leader::leader_manager::LeaderConfig;
@@ -13,22 +14,24 @@ use url::Url;
 
 #[derive(Debug, Parser)]
 pub struct Args {
-    #[arg(long, value_name = "FILE")]
-    pub staking_secret_key:   PathBuf,
-    #[arg(long, value_name = "FILE")]
-    pub bundle_key:           PathBuf,
-    #[arg(long, value_name = "FILE")]
-    pub submission_key:       PathBuf,
+    // #[arg(long, value_name = "FILE")]
+    // pub staking_secret_key:   PathBuf,
+    // #[arg(long, value_name = "FILE")]
+    // pub bundle_key:           PathBuf,
+    // #[arg(long, value_name = "FILE")]
+    // pub submission_key:       PathBuf,
+    // #[arg(long, value_delimiter = ',')]
+    // pub bootnodes:            Option<Vec<NodeRecord>>,
+    // #[arg(long, value_delimiter = ',')]
+    // pub builder_submissions:  Option<Vec<Url>>,
+    // #[arg(long)]
+    // pub bundle_sim_url:       Option<Url>,
     #[arg(long, default_value = "false")]
     pub enable_subscriptions: bool,
-    #[arg(long, value_delimiter = ',')]
-    pub bootnodes:            Option<Vec<NodeRecord>>,
-    #[arg(long, value_delimiter = ',')]
-    pub builder_submissions:  Option<Vec<Url>>,
     #[arg(long)]
-    pub bundle_sim_url:       Option<Url>,
+    pub full_node:      PathBuf,
     #[arg(long)]
-    pub full_node:            Url
+    pub full_node_ws: Url
 }
 
 impl Args {
@@ -38,7 +41,14 @@ impl Args {
         let fake_edsca = LocalWallet::new(&mut rand::thread_rng());
         let fake_bundle = LocalWallet::new(&mut rand::thread_rng());
 
-        let middleware = Box::leak(Box::new(Provider::new(Http::new(self.full_node))));
+        let inner = Provider::new(Http::new(self.full_node_ws));
+
+        let middleware = Box::leak(Box::new(RethMiddleware::new(
+            inner,
+            self.full_node,
+            tokio::runtime::Handle::current(),
+            1
+        ).unwrap()));
         let (sim, handle) = spawn_revm_sim(middleware, 6942069);
 
         let network_config = NetworkConfig::new(fake_key);
