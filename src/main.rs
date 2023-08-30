9c1f9af7d9660d3527d2ff48e8b0629167bc21af
lagrange.eth <lagrange@sorellacapital.com>
2023-08-29 23:39:47 -0700

    fix

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ use ethers_providers::{Http, Provider};
 use ethers_reth::RethMiddleware;
 use ethers_signers::LocalWallet;
 use guard_network::{config::SecretKey, NetworkConfig, PeersConfig};
+use hex_literal::hex;
 use leader::leader_manager::LeaderConfig;
-use reth_primitives::{mainnet_nodes, NodeRecord, H512};
+use reth_primitives::{mainnet_nodes, NodeRecord, PeerId, H512};
 use sim::spawn_revm_sim;
 use stale_guard::{Guard, SubmissionServerConfig};
 use tokio::runtime::{Handle, Runtime};
@@ -42,7 +43,10 @@ impl Args {
         let fake_key =
             SecretKey::from_str("046cfcdbef4955744de5f87e739883e7ffa5daa05945bda2b7f5d4b3123935de")
                 .unwrap();
-        let fake_pub_key = "04a3905ec9415c386d249b9bc9e430ce47c2f0e9dff67f749042dd2e58b24c3dda4e77f6c6c93d9b5d6377d63dd76c7e51e75057b7c3ff2b39f70027dcd50e80eb";
+        let mut fake_pub_key = hex!("04a3905ec9415c386d249b9bc9e430ce47c2f0e9dff67f749042dd2e58b24c3dda4e77f6c6c93d9b5d6377d63dd76c7e51e75057b7c3ff2b39f70027dcd50e80eb").to_vec()
+        fake_pub_key.push(0);
+        let fake_pub_key: &[u8; 65] = fake_pub_key.as_slice().try_into().unwrap();
+        
 
         let fake_edsca = LocalWallet::new(&mut rand::thread_rng());
         let fake_bundle = LocalWallet::new(&mut rand::thread_rng());
@@ -71,7 +75,8 @@ impl Args {
 
         let sim = spawn_revm_sim(db, 6942069);
 
-        let network_config = NetworkConfig::new(fake_key, fake_pub_key.parse().unwrap());
+        //let fake_pub_key: PeerId = fake_pub_key.into();
+        let network_config = NetworkConfig::new(fake_key, fake_pub_key.into());
         let leader_config = LeaderConfig {
             simulator: sim,
             edsca_key: fake_edsca,
