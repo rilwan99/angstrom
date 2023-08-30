2d4873b44d948b39fc48fa47562cae540c9b1b35
lagrange.eth <lagrange@sorellacapital.com>
2023-08-30 00:25:37 -0700

    test node 2

--- src/main.rs
+++ src/main.rs
@@ -32,18 +32,18 @@ pub struct Args {
     #[arg(long, default_value = "false")]
     pub enable_subscriptions: bool,
     #[arg(long)]
-    pub full_node:            PathBuf,
+    pub full_node: PathBuf,
     #[arg(long)]
-    pub full_node_ws:         String
+    pub full_node_ws: String,
 }
 
 impl Args {
     pub fn run(self, rt: Runtime) -> anyhow::Result<()> {
         //let fake_key = SecretKey::new(&mut rand::thread_rng());
         let fake_key =
-            SecretKey::from_str("046cfcdbef4955744de5f87e739883e7ffa5daa05945bda2b7f5d4b3123935de")
+            SecretKey::from_str("ad21c16051f74f24b3fbad57b0010d98bfef20441c84ee5a872133f19f807fc4")
                 .unwrap();
-        let fake_pub_key: Vec<u8>= hex!("04a3905ec9415c386d249b9bc9e430ce47c2f0e9dff67f749042dd2e58b24c3dda4e77f6c6c93d9b5d6377d63dd76c7e51e75057b7c3ff2b39f70027dcd50e80eb").to_vec();
+        let fake_pub_key: Vec<u8>= hex!("04b80d553719877f1aac5f60b816300cd26ba35bf9275e5105400aa5edfc0b69256f920019187647446ecd24fbc8a7714ef580b76ab14a8185a3370426fa6df9d8").to_vec();
         let fake_pub_key = fake_pub_key.as_slice();
 
         let fake_pub_key: &[u8; 64] =
@@ -57,16 +57,16 @@ impl Args {
         let middleware: &mut SignerMiddleware<Provider<Http>, LocalWallet> =
             Box::leak(Box::new(SignerMiddleware::new(
                 inner,
-                "046cfcdbef4955744de5f87e739883e7ffa5daa05945bda2b7f5d4b3123935de"
+                "ad21c16051f74f24b3fbad57b0010d98bfef20441c84ee5a872133f19f807fc4"
                     .parse()
-                    .unwrap()
+                    .unwrap(),
             )));
 
         let db_path = self.full_node.as_ref();
         let db = Arc::new(reth_db::mdbx::Env::<reth_db::mdbx::WriteMap>::open(
             db_path,
             reth_db::mdbx::EnvKind::RO,
-            None
+            None,
         )?);
 
         let sim = spawn_revm_sim(db, 6942069);
@@ -77,14 +77,14 @@ impl Args {
             simulator: sim,
             edsca_key: fake_edsca,
             bundle_key: fake_bundle,
-            middleware
+            middleware,
         };
 
-        let fake_addr = "ws://127.0.0.1:6969".parse()?;
+        let fake_addr = "ws://127.0.0.1:6970".parse()?;
         let server_config = SubmissionServerConfig {
-            addr:                fake_addr,
-            cors_domains:        "balls".into(),
-            allow_subscriptions: self.enable_subscriptions
+            addr: fake_addr,
+            cors_domains: "balls".into(),
+            allow_subscriptions: self.enable_subscriptions,
         };
 
         let guard = rt.block_on(Guard::new(network_config, leader_config, server_config));
