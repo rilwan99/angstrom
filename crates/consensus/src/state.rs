51f0fc23f217cb3eaa114caba80675e3a5f67094
Will Smith <williamqsmith11@gmail.com>
2023-09-26 18:44:46 -0700

    wip

--- crates/consensus/src/state.rs
+++ crates/consensus/src/state.rs
@@ -1,4 +1,10 @@
+use guard_provider::traits::{GuardSetWriter, RewardsWriter, StateWriter};
+use reth_db::mdbx::{tx::Tx, Env, WriteMap, RO};
+
+use super::unfinalized::UnfinalizedBlockQueue;
+
 pub struct ChainMaintainer<T: GuardSetWriter + RewardsWriter + StateWriter> {
+    reth_db:           Env<WriteMap>,
     unfinalized_queue: UnfinalizedBlockQueue,
     provider:          T
 }
@@ -7,7 +13,9 @@ impl<T> ChainMaintainer<T>
 where
     T: GuardSetWriter + RewardsWriter + StateWriter
 {
-    pub fn on_new_finalized_eth_block(&mut self, block: u64) {}
+    pub fn on_new_finalized_eth_block(&mut self, block: u64) {
+        let possible_blocks = self.unfinalized_queue.new_finalized_ethereum_block(block);
+    }
 
     pub fn on_new_height(&mut self, block: Block) {
         self.0.unfinalized_queue.push_back(block);
