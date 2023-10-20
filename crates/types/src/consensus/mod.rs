571a012c1e7015713466b8102f5ccc9a180ec71a
Will Smith <williamqsmith11@gmail.com>
2023-10-20 16:15:32 -0700

    wip

--- crates/types/src/consensus/mod.rs
+++ crates/types/src/consensus/mod.rs
@@ -1,6 +1,5 @@
 pub mod block;
 pub mod bundle;
-pub mod evidence;
 pub mod genesis;
 pub mod guard;
 pub mod proposal;
@@ -8,7 +7,6 @@ pub mod time;
 
 pub use block::*;
 pub use bundle::*;
-pub use evidence::*;
 pub use genesis::*;
 pub use guard::*;
 pub use proposal::*;
