71f7e3a1af1a54ac6e2051f6960add9195fcf18a
Will Smith <williamqsmith11@gmail.com>
2023-10-20 18:54:25 -0700

    fix network types

--- crates/guard-net/guard-network/src/messages.rs
+++ crates/guard-net/guard-network/src/messages.rs
@@ -7,11 +7,13 @@ use futures::FutureExt;
 use guard_eth_wire::{message::RequestPair, EthMessage};
 use guard_types::{
     consensus::{
-        Block, Bundle23Votes, BundleVote, GuardInfo, LeaderProposal, SignedLeaderProposal,
-        Valid23Bundle
+        Block, Bundle23Votes, BundleVote, GuardInfo, LeaderProposal, PrePreposeBundle,
+        ProposalCommit, SignedLeaderProposal, Valid23Bundle
     },
     database::State,
-    on_chain::{SimmedBundle, SimmedLvrSettlement, SimmedUserSettlement}
+    on_chain::{
+        SimmedBundle, SimmedLvrSettlement, SimmedUserSettlement, SubmittedOrder, VanillaBundle
+    }
 };
 use reth_interfaces::p2p::error::RequestResult;
 use tokio::sync::{oneshot, oneshot::Sender as OneSender};
@@ -20,24 +22,11 @@ use tokio::sync::{oneshot, oneshot::Sender as OneSender};
 #[derive(Debug, Clone)]
 pub enum PeerMessages {
     // Consensus related messages
-    /// new vote for a bundle
-    BundleVote(Arc<BundleVote>),
-    /// bundle that has 2/3
-    Bundle23Vote(Arc<Valid23Bundle>),
-    /// proposer block
-    LeaderProposal(Arc<LeaderProposal>),
-    /// signed leader proposal
-    SignedLeaderProposal(Arc<SignedLeaderProposal>),
-    /// new block that the network finalized on
-    NewBlock(Arc<Block>),
-    /// the state correlated to the underlying block
-    NewState(Arc<State>),
+    PrePropose(Arc<PrePreposeBundle>),
+    Proposal(Arc<LeaderProposal>),
+    Commit(Arc<ProposalCommit>),
 
-    // default propagation messages
-    /// new simmed user txes
-    PropagateUserTransaction(Arc<SimmedUserSettlement>),
-    /// new simmed searcher txes
-    PropagateSearcherTransaction(Arc<SimmedLvrSettlement>),
-    /// propagates a new bundle
-    PropagateBundle(Arc<SimmedBundle>)
+    // default communication
+    PropagateOrder(Arc<SubmittedOrder>),
+    PropagateBundle(Arc<VanillaBundle>)
 }
