a8daca50bfd2f5e30a2031dd0e9694c0ec05ac53
Will Smith <williamqsmith11@gmail.com>
2023-11-17 17:45:54 -0500

    wip

--- crates/order-pool/src/pool/limit/mod.rs
+++ crates/order-pool/src/pool/limit/mod.rs
@@ -17,10 +17,11 @@ pub trait LimitTx: Side {
     fn get_type(&self) -> OrderType;
     fn is_valid(&self) -> bool;
     fn is_expired(&self) -> bool;
+    fn is_composable(&self) -> bool;
     fn get_id(&self) -> TransactionId;
 }
 
-#[derive(Debug, Clone, Copy, Hash)]
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub struct TransactionId {
     pub user_addr:  Address,
     /// Hash of the order. Needed to check for inclusion
@@ -37,8 +38,10 @@ pub enum LimitPoolError {
     MaxSize,
     #[error("No pool was found for address: {0}")]
     NoPool(PoolId),
-    #[error("already have a ordered with {0:?}")]
-    DuplicateNonce(TransactionId)
+    #[error("Already have a ordered with {0:?}")]
+    DuplicateNonce(TransactionId),
+    #[error("Duplicate order")]
+    DuplicateOrder
 }
 
 pub enum LimitOrderLocation {
@@ -55,32 +58,55 @@ struct SizeTracker {
 }
 
 pub struct LimitOrderPool<T: LimitTx> {
-    composable_orders: ComposableLimitPool<T>,
-    limit_orders:      LimitPool<T>,
+    composable_orders:   ComposableLimitPool<T>,
+    limit_orders:        LimitPool<T>,
     /// used for easy update operations on Orders.
-    all_order_ids:     HashMap<TransactionId, LimitOrderLocation>,
+    all_order_ids:       HashMap<TransactionId, LimitOrderLocation>,
     /// used for nonce lookup.
-    user_to_id:        HashMap<Address, TransactionId>,
-    size:              SizeTracker
+    user_to_id:          HashMap<Address, Vec<TransactionId>>,
+    /// hash to location
+    order_hash_location: HashMap<B256, LimitOrderLocation>,
+    size:                SizeTracker
 }
 
 impl<T: LimitTx> LimitOrderPool<T> {
     pub fn new(max_size: Option<usize>) -> Self {
         Self {
-            composable_orders: ComposableLimitPool::new(),
-            limit_orders:      LimitPool::new(),
-            all_order_ids:     HashMap::new(),
-            user_to_id:        HashMap::new(),
-            size:              SizeTracker { max: max_size, current: 0 }
+            composable_orders:   ComposableLimitPool::new(),
+            limit_orders:        LimitPool::new(),
+            all_order_ids:       HashMap::new(),
+            user_to_id:          HashMap::new(),
+            order_hash_location: HashMap::new(),
+            size:                SizeTracker { max: max_size, current: 0 }
         }
     }
 
     pub fn new_order(&mut self, order: T) -> Result<(), LimitPoolError> {
+        let id = order.get_id();
+
+        // is new order
+        if self.all_order_ids.contains_key(&id) {
+            return Err(LimitPoolError::DuplicateOrder)
+        }
+
+        // check for duplicate nonce
+        if self
+            .user_to_id
+            .get(&id.user_addr)
+            .map(|inner| inner.iter().any(|other_id| other_id.nonce == id.nonce))
+            .unwrap_or(false)
+        {
+            return Err(LimitPoolError::DuplicateNonce(id))
+        }
+
+        // TODO: based on composability, insert into pools and then add to the tracking
+        // list
+
         Ok(())
     }
 
     pub fn filled_orders(&mut self, orders: &Vec<B256>) -> Vec<T> {
-        self.composable_orders.filled_orders(orders)
+        todo!()
     }
 
     pub fn get_all_order(&mut self) -> Vec<T> {
