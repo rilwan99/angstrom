103ec95b7f7566e123510ae3dd818eb3659ffc61
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-11-21 21:11:18 -0500

    removed depreciated types

--- crates/types/src/orders/origin.rs
+++ crates/types/src/orders/origin.rs
@@ -1,20 +1,16 @@
 /// Where the transaction originates from.
-///
-/// Depending on where the transaction was picked up, it affects how the
-/// transaction is handled internally, e.g. limits for simultaneous transaction
-/// of one sender.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum OrderOrigin {
-    /// Transaction is coming from a local source.
+    /// Order is coming from a local source.
     Local,
-    /// Transaction has been received externally.
+    /// Order has been received externally.
     ///
     /// This is usually considered an "untrusted" source, for example received
     /// from another in the network.
     External,
-    /// Transaction is originated locally and is intended to remain private.
-    ///
-    /// This type of transaction should not be propagated to the network. It's
-    /// meant for private usage within the local node only.
+    /// Order originated locally and is intended to remain private.
+    /// This type of Order should not be propagated to the network. It's
+    /// meant for private usage within the local node, or other composable
+    /// mev-guards.
     Private
 }
