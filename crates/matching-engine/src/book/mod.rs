7b039f4d234021330f7f1929bdc8d918d0f090c7
Philippe Dumonet <philippe@dumo.net>
2024-05-01 23:52:42 -0400

    🚧 order matching AMM + order book

--- crates/matching-engine/src/book/mod.rs
+++ crates/matching-engine/src/book/mod.rs
@@ -1,67 +1,178 @@
 //! basic book impl so we can benchmark
 
+use malachite::num::basic::traits::Zero;
+
 use crate::cfmm::UniswapV4Book;
-use malachite::num::basic::traits::{Two, Zero};
 
-pub trait BookOrder: Ord {
-    fn get_amount(&self) -> f64;
-    fn get_limit_price(&self) -> f64;
-    fn set_amount(&mut self, am: f64);
-    fn set_price(&mut self, price: f64);
+#[derive(Debug)]
+pub struct Order {
+    amount: f64,
+    price:  f64
 }
 
-pub trait SearcherOrder {
-    fn get_amount(&self) -> f64;
-    fn swap_direction(&self) -> bool;
+impl Order {
+    pub fn new(amount: f64, price: f64) -> Self {
+        Self { amount, price }
+    }
 }
 
-// TODO: just placeholder, will be more complex, just for prelim benchmarking
-pub struct BigBoyBook<const BOUND: usize, S: SearcherOrder, O: BookOrder> {
-    underlying_book: UniswapV4Book<BOUND>,
-    searcher_order:  S,
-    bids:            Vec<O>,
-    asks:            Vec<O>
+trait OrderList {
+    fn next_limit(&self) -> Option<f64>;
 }
 
-type PriceQty = (f64, f64);
+impl OrderList for Vec<Order> {
+    fn next_limit(&self) -> Option<f64> {
+        let order = self.last()?;
+        Some(order.price)
+    }
+}
 
-impl<const BOUND: usize, S: SearcherOrder, O: BookOrder> BigBoyBook<BOUND, S, O> {
-    pub fn fill_me(mut self) -> Option<PriceQty> {
+#[derive(Debug)]
+pub struct BigBoyBook<const BOUND: usize> {
+    amm:  UniswapV4Book<BOUND>,
+    bids: Vec<Order>,
+    asks: Vec<Order>
+}
+
+impl<const BOUND: usize> BigBoyBook<BOUND> {
+    pub fn new(amm: UniswapV4Book<BOUND>, bids: Vec<Order>, asks: Vec<Order>) -> Self {
+        assert!(
+            bids.windows(2).all(|w| w[0].price <= w[1].price)
+                && asks.windows(2).all(|w| w[0].price >= w[1].price)
+        );
+        BigBoyBook { amm, bids, asks }
+    }
+
+    pub fn fill_me(&mut self) -> (Option<f64>, f64) {
         let mut qty = f64::ZERO;
-        let mut p = f64::ZERO;
+        let mut p = None;
 
-        let mut a_idx = 0usize;
-        let mut b_idx = 0usize;
+        let mut next_bid = self.get_next_bid();
+        let mut next_ask = self.get_next_ask();
 
-        while self.asks.get(a_idx)?.get_limit_price() <= self.bids.get(b_idx)?.get_limit_price() {
-            let a = &mut self.asks[a_idx];
-            let b = &mut self.bids[b_idx];
+        while let Some((ask, bid)) = next_ask.as_mut().zip(next_bid.as_mut()) {
+            println!("\n");
+            dbg!(&ask);
+            dbg!(&bid);
+            println!("self.amm.get_price(): {}", self.amm.get_price());
 
-            let matched = std::cmp::min(a.get_amount(), b.get_amount());
+            let matched = ask.amount.min(bid.amount);
             qty += &matched;
-            let excess = b.get_amount() - a.get_amount();
+            let excess = bid.amount - ask.amount;
 
-            if excess == f64::ZERO {
-                p = (a.get_limit_price() + b.get_limit_price()) / f64::TWO;
-                a_idx += 1;
-                b_idx += 1;
-            } else if excess > f64::ZERO {
-                p = b.get_limit_price();
-                b.set_price(p.clone());
-                b.set_amount(b.get_amount() - matched);
-                a_idx += 1;
+            if excess > f64::ZERO {
+                p = Some(bid.price);
+                bid.amount -= matched;
+                next_ask = self.get_next_ask();
+                println!("fetching new ask: {:?}", &next_ask);
+            } else if excess < f64::ZERO {
+                p = Some(ask.price);
+                ask.amount -= matched;
+                next_bid = self.get_next_bid();
+                println!("fetching new bid: {:?}", &next_bid);
             } else {
-                p = a.get_limit_price();
-                a.set_price(p.clone());
-                a.set_amount(a.get_amount() - matched);
-                b_idx += 1;
+                p = Some(Self::avg_price(ask.price, bid.price));
+                next_bid = self.get_next_bid();
+                next_ask = self.get_next_ask();
             }
         }
 
-        let rem =
+        (p, qty)
+    }
 
+    fn avg_price(a: f64, b: f64) -> f64 {
+        (a * b).sqrt()
+    }
 
+    // Solve: next bid & ask would be AMM *but* pending non-emtpy order is limit =>
+    // gib AMM discretized limit order.
+    fn get_next_bid(&mut self) -> Option<Order> {
+        let (_, bid_is_amm) = self.next_bid_price();
+        let (ask_price, ask_is_amm) = self.next_ask_price();
 
-        Some((p, qty))
+        if ask_is_amm && bid_is_amm {
+            return None;
+        }
+
+        if bid_is_amm {
+            let limit = self.bids.next_limit().unwrap_or(ask_price);
+            let bid_amm = self.amm_to_order(limit);
+            dbg!(&bid_amm);
+            return Some(bid_amm);
+        }
+
+        self.bids.pop()
+    }
+
+    fn get_next_ask(&mut self) -> Option<Order> {
+        let (bid_price, bid_is_amm) = self.next_bid_price();
+        let (_, ask_is_amm) = self.next_ask_price();
+
+        if ask_is_amm && bid_is_amm {
+            return None;
+        }
+
+        if ask_is_amm {
+            let limit = self.asks.next_limit().unwrap_or(bid_price);
+            let ask_amm = self.amm_to_order(limit);
+            dbg!(&ask_amm);
+            return Some(ask_amm);
+        }
+
+        self.asks.pop()
+    }
+
+    fn amm_to_order(&mut self, limit: f64) -> Order {
+        // TODO: Proper error handling.
+        let (dx, dy) = self
+            .amm
+            .swap_to_limit(limit)
+            .expect("Read out-of-bound tick");
+        Order { amount: dx.abs(), price: -(dy / dx) }
+    }
+
+    fn next_bid_price(&self) -> (f64, bool) {
+        let amm_price = self.amm.get_price();
+        match self.bids.next_limit() {
+            Some(limit) if limit >= amm_price => (limit, false),
+            _ => (amm_price, true)
+        }
+    }
+
+    fn next_ask_price(&self) -> (f64, bool) {
+        let amm_price = self.amm.get_price();
+        match self.asks.next_limit() {
+            Some(limit) if limit <= amm_price => (limit, false),
+            _ => (amm_price, true)
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::cfmm::PoolRange;
+
+    #[test]
+    fn test_matching() {
+        println!("\nmatching");
+        let ranges = [
+            PoolRange::new(2000.0, 2100.0, 1000.0),
+            PoolRange::new(2100.0, 2200.0, 1000.0),
+            PoolRange::new(2200.0, 2300.0, 1000.0),
+            PoolRange::new(2300.0, 2400.0, 1000.0),
+            PoolRange::new(2400.0, 2500.0, 1000.0)
+        ];
+
+        let amm = UniswapV4Book::new(2250.0, 2, ranges);
+
+        let mut book =
+            BigBoyBook::new(amm, vec![Order::new(1.4, 2240.0)], vec![Order::new(2.4, 1800.0)]);
+
+        let out = book.fill_me();
+
+        println!("out: {:?}", out);
+
+        dbg!(&book);
     }
 }
--- crates/matching-engine/src/cfmm/mod.rs
+++ crates/matching-engine/src/cfmm/mod.rs
@@ -1,191 +1,144 @@
 //! The Underlying V4 cfmm curve. Transformed into a clob with N ticks
 //! on each side.
 
-#![allow(unused)]
+use malachite::num::basic::traits::Zero;
 
-use core::panic;
-
-use alloy_primitives::Address;
-use malachite::num::arithmetic::traits::{CheckedSqrt, FloorSqrt, Pow, Sqrt};
-use malachite_q::arithmetic::sqrt;
-
-/// Tick that is split in its price
-struct ActiveTick {
-    inner: PassiveTick,
-    price: f64
-}
-
-impl ActiveTick {
-    fn get_x_comp_at_price(&self, price: f64) -> f64 {
-        let b = self.price.pow(0.5);
-        (self.inner.liquidity
-            * (-price * self.inner.upper.sqrt() + self.inner.upper * price.sqrt()))
-            / (price * self.inner.upper)
-    }
-
-    fn get_y_comp_at_price(&self, price: f64) -> f64 {
-        -self.inner.liquidity * self.inner.lower.checked_sqrt()
-            + self.inner.liquidity * price.sqrt()
-    }
-
-    /// amount to sell to reach tick edge
-    fn full_sell_amount(&self) -> f64 {
-        self.inner.y_given_x(0.0) - self.inner.y_given_x(self.get_x_comp_at_price(self.price))
-    }
-
-    /// amount to buy to reach tick edge
-    fn full_buy_amount(&self) -> f64 {
-        self.inner.x_given_y(0.0) - self.inner.x_given_y(self.get_y_comp_at_price(self.price))
-    }
-
-    /// the new price of the tick given we are buying some amount.
-    /// NOTE: this assumes tick overflow checks have been done
-    pub fn new_price_buy(&self, amount: f64) -> f64 {
-        // buy token 0
-        let dx = self.get_x_comp_at_price(self.price) + amount;
-        // token 1 sell
-        let dy = self.get_y_comp_at_price(self.price) - self.inner.y_given_x(dx);
-        self.inner.price(dx, dy)
-    }
-
-    /// the new price of the tick given we are buying some amount.
-    /// NOTE: this assumes tick overflow checks have been done
-    pub fn new_price_sell(&self, amount: f64) -> f64 {
-        // buy token 0
-        let dy = self.get_y_comp_at_price(self.price) + amount;
-        // token 1 sell
-        let dx = self.get_x_comp_at_price(self.price) - self.inner.x_given_y(dy);
-        self.inner.price(dx, dy)
-    }
-}
-
-/// Tick that isn't split in its price
-struct PassiveTick {
-    // tick bounds
+/// Starts and ends at ticks but might not be a single tick if it spans
+/// uninitialized ticks.
+#[derive(Debug)]
+pub struct PoolRange {
+    // Liquidity range bounds.
     lower:     f64,
     upper:     f64,
-    liquidity: f64
+    liquidity: f64,
+    // (𝛥x, 𝛥y) The the net amounts required to fully jump the range.
+    full_dx:   f64,
+    full_dy:   f64
 }
 
-impl PassiveTick {
-    /// the x component for the given tick.
-    fn x_given_y(&self, y: f64) -> f64 {
-        (-self.lower.sqrt() * self.liquidity.powi(2) + (self.upper.sqrt() * self.liquidity.powi(2))
-            - self.liquidity * y)
-            / (self.upper.sqrt() * (self.lower.sqrt() * self.liquidity + y))
+impl PoolRange {
+    pub fn new(lower: f64, upper: f64, liquidity: f64) -> Self {
+        // TODO: Use Result.
+        assert!(upper > lower);
+        assert!(lower > f64::ZERO);
+        assert!(liquidity > f64::ZERO);
+        let full_dx = (1.0 / lower.sqrt() - 1.0 / upper.sqrt()) * liquidity;
+        let full_dy = (lower.sqrt() - upper.sqrt()) * liquidity;
+        Self { lower, upper, liquidity, full_dx, full_dy }
     }
 
-    fn y_given_x(&self, x: f64) -> f64 {
-        ((-self.lower.sqrt() * self.upper.sqrt() * self.liquidity * x)
-            - (self.lower.sqrt() * self.liquidity.powi(2)
-                + self.upper.sqrt() * self.liquidity.powi(2)))
-            / (self.upper.sqrt() * x + self.liquidity)
+    fn partial_dx(&self, from_price: f64, to_price: f64) -> f64 {
+        (1.0 / to_price.sqrt() - 1.0 / from_price.sqrt()) * self.liquidity
     }
 
-    fn price(&self, x: f64, y: f64) -> f64 {
-        (y + self.liquidity * self.lower.sqrt()) / ((self.liquidity / self.upper.sqrt()) + x)
-    }
-
-    fn full_buy_amount(&self) -> f64 {
-        self.y_given_x(0.0)
-    }
-
-    /// NOTE: assumes tick is at the upper tick
-    fn partial_buy_price(&self, amount: f64) -> f64 {
-        // because x at zero, dx = amount;
-        let dy = self.y_given_x(0.0) - self.y_given_x(amount);
-        self.price(amount, dy)
-    }
-
-    /// NOTE: assumes tick is at lower tick
-    fn partial_sell_price(&self, amount: f64) -> f64 {
-        let dx = self.x_given_y(0.0) - self.x_given_y(amount);
-        self.price(dx, amount)
+    fn partial_dy(&self, from_price: f64, to_price: f64) -> f64 {
+        (to_price.sqrt() - from_price.sqrt()) * self.liquidity
     }
 }
 
 /// UniswapV4 Ticks in a book format.
 /// Math: https://www.desmos.com/calculator/kh8ckngzap
+#[derive(Debug)]
 pub struct UniswapV4Book<const BOUND: usize> {
-    token0: Address,
-    token1: Address,
-    /// the current split tick
-    mid:    ActiveTick,
-    /// complete bid ticks,
-    bids:   [PassiveTick; BOUND],
-    /// complete ask ticks,
-    asks:   [PassiveTick; BOUND]
+    price:       f64,
+    range_index: usize,
+    ranges:      [PoolRange; BOUND]
 }
 
 impl<const BOUND: usize> UniswapV4Book<BOUND> {
-    /// returns the price for the amount bought
-    pub fn buy_tokens(&self, mut amount: f64) -> f64 {
-        if self.mid.full_buy_amount() > amount {
-            return (self.mid.price + self.mid.new_price_buy(amount)) / 2.0
-        } else {
-            let full_tick_am = self.mid.full_buy_amount();
-            let mut pxa = self.mid.new_price_buy(full_tick_am) * full_tick_am;
-            let mut weights = full_tick_am;
-            amount -= full_tick_am;
-
-            let mut tick_i = 0usize;
-
-            // while the liquidity in tick range is more than we need to clear
-            while self.asks[tick_i].x_given_y(0.0) >= amount {
-                let ask = &self.asks[tick_i];
-                tick_i += 1;
-
-                if tick_i == BOUND {
-                    panic!("out of bounds.. this will become error in future");
-                }
-
-                let full_tick_am = ask.x_given_y(0.0);
-                pxa += ((ask.lower + ask.upper) / 2.0) * full_tick_am;
-                weights += full_tick_am;
-            }
-
-            if amount > 0.0 {
-                let ask = &self.asks[tick_i];
-                pxa += ask.partial_buy_price(amount) * amount;
-                weights += amount;
-            }
-
-            pxa / weights
-        }
+    pub fn new(price: f64, range_index: usize, ranges: [PoolRange; BOUND]) -> Self {
+        assert!(range_index < BOUND);
+        assert!(ranges.windows(2).all(|w| w[0].upper == w[1].lower));
+        assert!(ranges[range_index].lower <= price && price <= ranges[range_index].upper);
+        Self { price, range_index, ranges }
     }
 
-    pub fn sell_tokens(&self, mut amount: f64) -> f64 {
-        if self.mid.full_sell_amount() > amount {
-            return (self.mid.price + self.mid.new_price_sell(amount)) / 2.0
-        } else {
-            let full_tick_am = self.mid.full_sell_amount();
-            let mut pxa = self.mid.new_price_sell(full_tick_am) * full_tick_am;
-            let mut weights = full_tick_am;
-            amount -= full_tick_am;
+    pub fn get_price(&self) -> f64 {
+        self.price
+    }
 
-            let mut tick_i = 0usize;
+    fn current(&self) -> Option<&PoolRange> {
+        self.ranges.get(self.range_index)
+    }
 
-            // while the liquidity in tick range is more than we need to clear
-            while self.bids[tick_i].y_given_x(0.0) >= amount {
-                let bid = &self.bids[tick_i];
-                tick_i += 1;
+    pub fn swap_to_limit(&mut self, limit: f64) -> Option<(f64, f64)> {
+        let mut dx: f64 = f64::ZERO;
+        let mut dy: f64 = f64::ZERO;
 
-                if tick_i == BOUND {
-                    panic!("out of bounds.. this will become error in future");
+        let mut current = &self.ranges[self.range_index];
+        let mut last_price = self.price;
+
+        if limit < current.lower {
+            // Swap to end of current.
+            dx += current.partial_dx(last_price, current.lower);
+            dy += current.partial_dy(last_price, current.lower);
+
+            loop {
+                self.range_index = self.range_index.checked_sub(1)?;
+                current = self.current()?;
+                if current.lower < limit {
+                    break;
                 }
-
-                let full_tick_am = bid.y_given_x(0.0);
-                pxa += ((bid.lower + bid.upper) / 2.0) * full_tick_am;
-                weights += full_tick_am;
+                dx += current.full_dx;
+                dy += current.full_dy;
+                last_price = current.lower;
             }
+        } else if current.upper < limit {
+            // Swap to end of current.
+            dx += current.partial_dx(last_price, current.upper);
+            dy += current.partial_dy(last_price, current.upper);
 
-            if amount > 0.0 {
-                let bid = &self.bids[tick_i];
-                pxa += bid.partial_sell_price(amount) * amount;
-                weights += amount;
+            loop {
+                self.range_index = self.range_index.checked_add(1)?;
+                current = self.current()?;
+                if limit < current.upper {
+                    break;
+                }
+                dx -= current.full_dx;
+                dy -= current.full_dy;
+                last_price = current.upper;
             }
-
-            pxa / weights
         }
+
+        dx += current.partial_dx(last_price, limit);
+        dy += current.partial_dy(last_price, limit);
+
+        self.price = limit;
+
+        Some((dx, dy))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_basic_range() {
+        println!("\ntest_basic_range");
+        let range = PoolRange::new(3000.0, 3100.0, 3400.0);
+        println!("range.full_dx: {}", range.full_dx);
+        println!("range.full_dy: {}", range.full_dy);
+
+        let dx = range.partial_dx(range.lower, 3050.0);
+        let dy = range.partial_dy(range.lower, 3050.0);
+
+        println!("dx: {}", dx);
+        println!("dy: {}", dy);
+
+        let ranges = [
+            PoolRange::new(2000.0, 2100.0, 1000.0),
+            PoolRange::new(2100.0, 2200.0, 1000.0),
+            PoolRange::new(2200.0, 2300.0, 1000.0),
+            PoolRange::new(2300.0, 2400.0, 1000.0),
+            PoolRange::new(2400.0, 2500.0, 1000.0)
+        ];
+
+        let mut book = UniswapV4Book::new(2250.0, 2, ranges);
+
+        let (dx, dy) = book.swap_to_limit(2200.0 - 1e-13).unwrap();
+        println!("dx: {}", dx);
+        println!("dy: {}", dy);
+        println!("book.price: {}", book.price);
+        println!("book.range_index: {}", book.range_index);
     }
 }
