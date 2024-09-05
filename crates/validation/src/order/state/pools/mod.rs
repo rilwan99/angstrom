5a1ef536a3d40ae2723347df1eeab72c8ad592b8
Will Smith <williamqsmith11@gmail.com>
2024-09-05 18:48:24 -0400

    pool mock

--- crates/validation/src/order/state/pools/mod.rs
+++ crates/validation/src/order/state/pools/mod.rs
@@ -114,3 +114,49 @@ pub mod pool_tracker_mock {
         }
     }
 }
+
+#[cfg(test)]
+pub mod pool_tracker_mock {
+    use alloy_primitives::{Address, FixedBytes};
+    use dashmap::DashMap;
+
+    use super::{angstrom_pools::PoolIdWithDirection, *};
+
+    #[derive(Clone)]
+    pub struct MockPoolTracker {
+        asset_index_to_address: DashMap<u16, Address>,
+        pools:                  DashMap<(Address, Address), PoolIdWithDirection>
+    }
+
+    impl MockPoolTracker {
+        pub fn add_asset(&self, index: u16, address: Address) {
+            self.asset_index_to_address.insert(index, address);
+        }
+
+        pub fn add_pool(&self, token0: Address, token1: Address, info: PoolIdWithDirection) {
+            self.pools.insert((token0, token1), info);
+        }
+    }
+
+    impl PoolsTracker for MockPoolTracker {
+        fn fetch_pool_info_for_order<O: RawPoolOrder>(
+            &self,
+            order: O
+        ) -> Option<(UserOrderPoolInfo, AssetIndexToAddressWrapper<O>)> {
+            let token_in = *self
+                .asset_index_to_address
+                .get(&order.get_token_in())?
+                .value();
+            let token_out = *self
+                .asset_index_to_address
+                .get(&order.get_token_out())?
+                .value();
+
+            let (is_bid, pool_id) = self.pools.get(&(token_in, token_out))?.value();
+            let wrapped = AssetIndexToAddressWrapper { token_out, token_in, order };
+            let info = UserOrderPoolInfo { pool_id, is_bid, token: token_in };
+
+            Some((info, wrapped))
+        }
+    }
+}
