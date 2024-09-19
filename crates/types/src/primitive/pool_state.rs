dd18516c4f894f82c56674bdc15211e311ae2233
jnoorchashm37 <joseph@sorellalabs.xyz>
2024-09-19 16:01:33 -0400

    updated pool

--- crates/types/src/primitive/pool_state.rs
+++ crates/types/src/primitive/pool_state.rs
@@ -10,23 +10,19 @@ pub type PoolIdWithDirection = (bool, PoolId);
 /// just a placeholder type so i can implement the general architecture
 #[derive(Debug, Clone, Copy)]
 pub struct NewInitializedPool {
-    pub currency_in:        Address,
-    pub currency_in_index:  u16,
-    pub currency_out:       Address,
-    pub currency_out_index: u16,
-    pub id:                 PoolIdWithDirection
+    pub currency_in:  Address,
+    pub currency_out: Address,
+    pub id:           PoolIdWithDirection
 }
 
 impl From<Log<Initialize>> for NewInitializedPool {
     fn from(value: Log<Initialize>) -> Self {
         // tf do the indexes come from
         Self {
-            currency_in:        value.currency0,
-            currency_in_index:  0,
-            currency_out:       value.currency1,
-            currency_out_index: 0,
+            currency_in:  value.currency0,
+            currency_out: value.currency1,
             // where is this generated
-            id:                 (true, 0)
+            id:           (true, 0)
         }
     }
 }
