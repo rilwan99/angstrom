480a9b9a8766d527d3b6b4e013f9fffd53ff1a43
Joseph Noorchashm <joseph@sorellalabs.xyz>
2024-09-24 15:56:07 -0400

    Update crates/order-pool/src/limit/standard.rs
    Co-authored-by: Will Smith <44074786+Will-Smith11@users.noreply.github.com>

--- crates/order-pool/src/limit/standard.rs
+++ crates/order-pool/src/limit/standard.rs
@@ -90,13 +90,11 @@ impl LimitPool {
         let old_is_none = self
             .pending_orders
             .insert(pool.id.1, PendingPool::new())
-            .is_none();
-        assert!(old_is_none);
-
-        let old_is_none = self
+            .is_none() || self
             .parked_orders
             .insert(pool.id.1, ParkedPool::new())
             .is_none();
+            
         assert!(old_is_none);
     }
 }
