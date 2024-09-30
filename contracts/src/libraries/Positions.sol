f71a8a84e2110db5338058bfb3cf8311c8bb6d38
philogy <philippe@dumo.net>
2024-09-29 18:31:22 -0700

    ♻️ Simplify positions

--- contracts/src/libraries/Positions.sol
+++ contracts/src/libraries/Positions.sol
@@ -4,9 +4,8 @@ pragma solidity ^0.8.13;
 import {PoolId} from "v4-core/src/types/PoolId.sol";
 import {POSITIONS_STORAGE_PREFIX} from "src/Constants.sol";
 
-/// @dev Custom `(PoolId id, address owner, int24 lowerTick, int24 upperTick, bytes32 salt) => Position` mapping
 struct Positions {
-    uint256 __placeholder;
+    mapping(PoolId id => mapping(bytes32 uniPositionKey => Position)) positions;
 }
 
 struct Position {
@@ -18,32 +17,26 @@ using PositionsLib for Positions global;
 /// @author philogy <https://github.com/philogy>
 library PositionsLib {
     function get(
-        Positions storage positions,
+        Positions storage self,
         PoolId id,
         address sender,
         int24 lowerTick,
         int24 upperTick,
         bytes32 salt
-    ) internal pure returns (Position storage position, bytes32 positionKey) {
+    ) internal view returns (Position storage position, bytes32 positionKey) {
         assembly ("memory-safe") {
             let free := mload(0x40)
 
             // Compute `positionKey` as `keccak256(abi.encodePacked(sender, lowerTick, upperTick, sender))`.
+            // Less efficient than alternative ordering *but* lets us have the position key.
             mstore(0x06, upperTick)
             mstore(0x03, lowerTick)
             mstore(0x00, sender)
             // WARN: Free memory pointer temporarily invalid from here on.
             mstore(0x26, salt)
-            positionKey := keccak256(12, add(add(20, 3), add(3, 32)))
-
-            // Compute sorage slot of position.
-            mstore(0x00, id)
-            mstore(0x20, positionKey)
-            mstore(0x40, positions.slot)
-            position.slot := or(shr(8, keccak256(0x00, 0x60)), shl(248, POSITIONS_STORAGE_PREFIX))
-
-            // Fee memory pointer restored.
-            mstore(0x40, free)
+            positionKey := keccak256(12, add(add(3, 3), add(20, 32)))
+            mstore(0x26, 0)
         }
+        position = self.positions[id][positionKey];
     }
 }
