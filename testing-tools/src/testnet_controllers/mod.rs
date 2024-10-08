5ecb642dffb4f25369dadfb0b8fb85fd54235ae4
jnoorchashm37 <joseph@sorellalabs.xyz>
2024-10-08 11:01:46 -0400

    tracing

--- testing-tools/src/testnet_controllers/mod.rs
+++ testing-tools/src/testnet_controllers/mod.rs
@@ -160,8 +160,7 @@ impl StromTestnet {
         };
 
         let peer = self.peers.get(&id).unwrap();
-        //let span = span!(Level::DEBUG, "testnet node", ?id);
-        let r = f(&peer).await; //.instrument(span).await;
-        r
+        let span = span!(Level::TRACE, "testnet node", ?id);
+        f(&peer).instrument(span).await
     }
 }
