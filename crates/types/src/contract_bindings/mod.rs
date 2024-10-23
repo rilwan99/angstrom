1c44cc0263ced2e1984cdf219f13886def2671ef
Plamen Hristov <phristov@blocksoft.ltd>
2024-10-23 12:18:13 +0300

    Reverted the contract_bindings changes

--- crates/types/src/contract_bindings/mod.rs
+++ crates/types/src/contract_bindings/mod.rs
@@ -1,9 +1,9 @@
-pub mod mintable_mock_erc_20 {
+pub mod pool_manager {
     alloy::sol!(
         #[allow(missing_docs)]
         #[sol(rpc)]
-        MintableMockERC20,
-        "../../contracts/out/MintableMockERC20.sol/MintableMockERC20.json"
+        PoolManager,
+        "../../contracts/out/PoolManager.sol/PoolManager.json"
     );
 }
 pub mod mock_rewards_manager {
@@ -14,12 +14,12 @@ pub mod mock_rewards_manager {
         "../../contracts/out/MockRewardsManager.sol/MockRewardsManager.json"
     );
 }
-pub mod pool_manager {
+pub mod angstrom {
     alloy::sol!(
         #[allow(missing_docs)]
         #[sol(rpc)]
-        PoolManager,
-        "../../contracts/out/PoolManager.sol/PoolManager.json"
+        Angstrom,
+        "../../contracts/out/Angstrom.sol/Angstrom.json"
     );
 }
 pub mod pool_gate {
@@ -30,11 +30,11 @@ pub mod pool_gate {
         "../../contracts/out/PoolGate.sol/PoolGate.json"
     );
 }
-pub mod angstrom {
+pub mod mintable_mock_erc_20 {
     alloy::sol!(
         #[allow(missing_docs)]
         #[sol(rpc)]
-        Angstrom,
-        "../../contracts/out/Angstrom.sol/Angstrom.json"
+        MintableMockERC20,
+        "../../contracts/out/MintableMockERC20.sol/MintableMockERC20.json"
     );
 }
